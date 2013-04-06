572309a4ddfb3007b968e99ed3fa7cd3f2e7cd1c
khirakawa <k.hirakawa88@gmail.com>
2013-04-06 17:21:22 -0400

    added receiver and transmitter ino files

---
+++ transmitter/transmitter.ino
@@ -0,0 +1,32 @@
+#include <VirtualWire.h>
+
+int switchPin = A5;
+
+void setup()
+{
+  Serial.begin(9600);	  // Debugging only
+  Serial.println("setup");
+
+  // Initialise the IO and ISR
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+  vw_set_tx_pin(6); 
+      
+  pinMode(switchPin, INPUT);
+  digitalWrite(switchPin, HIGH);
+}
+
+void loop()
+{
+  char *msg;
+  
+  if(digitalRead(A5) == LOW){
+    Serial.print("BUTTON PRESSED");
+    char *msg = "test";
+    digitalWrite(13, true); // Flash a light to show transmitting
+    vw_send((uint8_t *)msg, strlen(msg));
+    vw_wait_tx(); // Wait until the whole message is gone
+    digitalWrite(13, false);
+  }
+}
+  
