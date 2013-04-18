df0a4475110aa3b7fb61a16b7cc7d8479bf06019
khirakawa <k.hirakawa88@gmail.com>
2013-04-17 22:34:29 -0400

    initial app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+var arduino = require('duino');
+
+var board = new arduino.Board({
+	baudrate: 9600,
+	debug: true,
+	device: "usbmodemfa131"
+});
+
+board.on('ready', function(){
+	console.log("ready");
+
+	var rfReceiver = new arduino.rf.Receiver({
+		board: board,
+		pin: '02'
+	});
+
+	rfReceiver.on('read', function(err, data){
+		console.log("data", data);
+	});
+});
